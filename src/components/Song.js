66f819d47c826b3bc5d540b9241a934d9bb55b12
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-05-18 15:25:51 +0900

    デフォルトの音量調整

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const Song = (props) => {
+
+  const adjustAudioVol = () => {
+    const audio = document.querySelector("audio");
+    audio.volume = 0.3;
+  }
+  
   return (
     <div className="song">
       <p>{props.rank + 1}位</p>
@@ -8,7 +14,7 @@ const Song = (props) => {
       <img width="100px" height="100px" src={props.image} alt="" />
       {props.audio === null
         ? <></>
-        : <video width="200px" height="80px" controls preload="none" name="media" src={props.audio} type="audio/mpeg"></video>
+        : <audio controls preload="none" name="media" src={props.audio} type="audio/mpeg" onPlay={adjustAudioVol}></audio>
       }
       <a href={props.spotifyLink} target="_blank" rel="noopener noreferrer">フルで聞く</a>
     </div>
