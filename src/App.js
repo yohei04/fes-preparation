cba341de54c72b90edbd32e828e5a1aceb0a979b
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-05-20 15:30:21 +0900

    Scssファイルの切り分け

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './styles/App.scss';
-import './styles/reset.scss';
 import { Header, About, Search } from './components/index';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 
