128adc6a6b7601c9151344ca5a0a1d15463609b4
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-05-21 11:32:20 +0900

    いらないところ削除

--- src/App.js
+++ src/App.js
@@ -21,25 +21,3 @@ const App = () => {
 
 
 export default App;
-
-
-  // const getSongs = () => {
-  //   request.post(authOptions, function (error, response, body) {
-  //     if (!error && response.statusCode === 200) {
-  //       // use the access token to access the Spotify Web API
-  //       var token = body.access_token;
-  //       var options = {
-  //         url: `https://api.spotify.com/v1/artists/${spotifyArtistId}/top-tracks?country=JP`,
-  //         headers: {
-  //           'Authorization': 'Bearer ' + token
-  //         },
-  //         json: true
-  //       };
-
-  //       request.get(options, function (error, response, body) {
-  //         setSongs(body.tracks)
-  //         // console.log(songs)
-  //       });
-  //     }
-  //   });
-  // }
